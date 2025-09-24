c53a34c79553c72fecd6193675ef1a5312ea7cc1
Shantanu Negi <shantanu_negi@hotmail.com>
2025-09-24 20:45:36 +1000

    commenting.

--- SampleBlazorWebApp/Program.cs
+++ SampleBlazorWebApp/Program.cs
@@ -9,6 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+//Add WebRTC Blazor client
 builder.Services.AddWebRTCBlazorClient();
 
 await builder.Build().RunAsync();
