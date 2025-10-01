5ac7c18e691a43111463516ed69fc690a03cf66c
Shantanu Negi <shantanu_negi@hotmail.com>
2025-10-01 18:26:30 +1000

    created default route to video chat page in angular app.

--- sample-angular-web-app/src/app/app.routes.ts
+++ sample-angular-web-app/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { MyVideoChatComponent } from './pages/my-video-chat/my-video-chat.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/video-chat', pathMatch: 'full' },
     { path: 'video-chat', component: MyVideoChatComponent },
 ];
